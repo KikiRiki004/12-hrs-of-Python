0e63bc788831892f0d2a0ac8e335785e1e77ba93
KikiRiki004 <khanov.krl@gmail.com>
2024-03-24 15:30:31 +0100

    String methods

---
+++ #4 String Methods.py
@@ -0,0 +1,24 @@
+name = "bro Code"
+age = "20"
+line = "Line"
+
+print(name)
+
+print(len(name))
+print(name.find("r"))
+
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+
+print(name.isdigit())
+print(age.isdigit())
+
+print(name.isalpha())
+print(line.isalpha())
+
+print(name.count("o"))
+
+print(name.replace("o", "a"))
+
+print(age * 3)
